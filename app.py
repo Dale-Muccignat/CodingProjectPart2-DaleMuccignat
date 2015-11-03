ee91c12576d14cf95f4ecabb68d7a57fe29c7229
Dale-Muccignat <dale.muccignat@my.jcu.edu.au>
2015-11-03 17:10:58 +1000

    Fixed spinner, added today's date

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.properties import ListProperty
+import time
 from kivy.core.window import Window
 from currency import get_all_details
 
 
 class GuiTest(App):
     current_state = StringProperty()
+    home_state = StringProperty()
     country_names = ListProperty()
-
+    current_date = "  Today is: \n" + time.strftime("%Y/%m/%d")
 
     def __init__(self, **kwargs):
         super(GuiTest, self).__init__(**kwargs)
@@ -22,26 +24,25 @@ class GuiTest(App):
         country_list = GuiTest.sort_trips()
         self.title = "Part 2"
         self.root = Builder.load_file('gui.kv')
-        # print(country_list)
-        # print(country_list[0])
         # Set current state to first country in list
         self.current_state = str(country_list[0])
-        #
+        # Add values to spinner
         self.country_names = country_list
         return self.root
 
     # def update_button_press(self):
     #     print("update_button pressed")
-    #
-    def change_state(self):
-        self.root.ids.test_label.text = "woo"
-        print("changed")
+
+    def change_state(self, text):
+        # self.root.ids.test_label.text = text
+        print("country changed")
 
     @staticmethod
     def sort_trips():
         """ creates list of countries to display on spinner """
         trip_countries = []
         file = open("config.txt", mode="r", encoding="UTF-8")
+        GuiTest.home_state = file.readline()
         # Generates list of country names
         for line in file:
             parts = line.strip().split(",")
@@ -51,4 +52,3 @@ class GuiTest(App):
 
 
 GuiTest().run()
-# print(GuiTest().sort_trips())
